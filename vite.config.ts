61b7639c1b94c0eae00ea503ccaf111329a2b3f8
Priya Bagus Amanullah <priyabagus.a29@gmail.com>
2024-07-27 14:32:46 +0700

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "priya-bagus-amanullah",
+    project: "javascript-react"
+  })],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
